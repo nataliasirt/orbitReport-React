70f2301fde08e0b467710e029e84d41f37d95400
nsirtak <nsirtak@gmail.com>
2024-09-05 14:35:27 -0500

    fixes

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,8 +1,9 @@
 import satData from "./satData";
 import './styling.css';
-
+//Pass the props created from the App() function
 const Buttons = ({ filterByType, setSat, displaySats }) => {
   return (
+    //Update the first & second buttons
     <div className = "flex-container">
       {displaySats.map((sat, id) => {
         return(
@@ -11,7 +12,6 @@ const Buttons = ({ filterByType, setSat, displaySats }) => {
           </button>
         );
       })}
-
         <button onClick={() => setSat(satData)}>
           All Orbits
         </button>
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,33 +1,29 @@
 import './styling.css';
 
 const Table = ({ sat }) => {
-  const satData = sat.map((data) => {
-    const operational = data.operational ? "active" : "inactive";
-    return(
-    <tr key={data.id}>
-      <td>{data.name}</td>
-      <td>{data.type}</td>
-      <td>{data.launchDate}</td>
-      <td>{operational}</td>
-    </tr>
-    )
-  })
   return (
     <table>
-    <thead>
-     <tr>
-      <th>Name</th>
-      <th>Type of Satellite</th>
-      <th>Launch Date</th>
-      <th>Status</th>
-     </tr>
-     </thead>
-
-     <tbody>
-     {satData}
-     </tbody>
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Type of Satellite</th>
+                <th>Launch Date</th>
+                <th>Status</th>
+            </tr>
+        </thead>
+        <tbody>
+        //will use the map function to render the data based on which button a user selects
+        {sat.map((data) => (
+           <tr key= {data.id}>
+            <td>{data.name}</td> 
+            <td>{data.type}</td>
+            <td>{data.launchDate}</td>
+            <td>{data.operational ? "Operational": "Non-operational"}</td>
+            </tr>
+            ))}
+        </tbody>
    </table>
-  );
-};
+          );
+        };
 
 export default Table;
